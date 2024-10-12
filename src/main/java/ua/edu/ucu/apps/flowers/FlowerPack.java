741b82c3c02043d6fcb42c3444f72ae19438bc80
Viktor Pakholok <pakholok.viktor@ukr.net>
2024-10-12 19:50:11 +0300

    Implemented FlowerPack

---
+++ src/main/java/ua/edu/ucu/apps/flowers/FlowerPack.java
@@ -0,0 +1,16 @@
+package ua.edu.ucu.apps.flowers;
+
+
+public class FlowerPack {
+    private int quantity;
+    private Flower flower;
+
+    public FlowerPack(Flower flower, int quantity) {
+        this.flower = new Flower(flower);
+        this.quantity = quantity;
+    }
+
+    public double getPrice() {
+        return flower.getPrice() * quantity;
+    }
+}
